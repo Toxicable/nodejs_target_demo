353b26de67700fee56a170e760c163228a10db52
Fabian Wiles <fabianwiles@live.com>
2018-04-08 10:23:40 +1200

    inital

---
+++ projects/app1/src/main.ts
@@ -0,0 +1,9 @@
+console.log('Hello World2');
+setTimeout(() => {
+  debugger;
+  const sadfasdfsda = '';
+}, 2000)
+
+var t= 3;
+3 + 3;
+t = ''
---
+++ update.sh
@@ -0,0 +1,8 @@
+rm build-webpack-nodejs.tgz
+
+cd ../devkit 
+yarn build
+cd dist/@angular-devkit/build-webpack-nodejs
+yarn
+cd ../../../
+cp dist/@angular-devkit_build-webpack-nodejs.tgz ../nodejs_target_demo/webpack-nodejs.tgz
