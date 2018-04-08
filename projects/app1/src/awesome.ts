c726178ca137918873e07fecca49e8c25ebb10e5
Fabian Wiles <fabianwiles@live.com>
2018-04-08 20:01:16 +1200

    add a few more plugins

--- projects/app1/src/awesome.ts
+++ projects/app1/src/awesome.ts
@@ -3,8 +3,7 @@ import * as express from 'express';
 
 const app = express();
 
-console.log(__dirname)
 
-app.get('/', (req, res) => res.send('Hello World!2'))
+app.get('/', (req, res) => res.send('Hello Wo  r ld!2'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
