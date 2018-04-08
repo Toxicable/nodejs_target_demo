ecac1baef02bb344566719bed6862322320c3e97
Fabian Wiles <fabianwiles@live.com>
2018-04-08 19:12:10 +1200

    improve webpack config

--- projects/app1/src/awesome.ts
+++ projects/app1/src/awesome.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 
 const app = express();
 
+console.log(__dirname)
+
 app.get('/', (req, res) => res.send('Hello World!2'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
