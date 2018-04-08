bc85db407335b4c5ade6f92bd4efbe06c73d9acf
Fabian Wiles <fabianwiles@live.com>
2018-04-08 21:17:16 +1200

    show error

--- projects/app1/src/awesome.ts
+++ projects/app1/src/awesome.ts
@@ -3,7 +3,16 @@ import * as express from 'express';
 
 const app = express();
 
+const t: string = 'a'
 
-app.get('/', (req, res) => res.send('Hello Wo  r ld!2'))
+app.get('/', (req, res) => {
+
+    if(t){
+        throw new Error('helo');
+    }
+
+    res.send('Hello Wo  r ld!2')}
+
+)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
